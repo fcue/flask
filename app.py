0359137b2213bbefd37858723f4f741281fb82ee
fcue <cuefrances@gmail.com>
2020-04-08 20:40:01 -0400

    chore: add config.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+import os
+
 
 app = Flask(__name__)
+# set up app configuration
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
@@ -14,3 +18,4 @@ def hello_name(name):
 if __name__ == '__main__':
     app.run()
 
+print(os.environ['APP_SETTINGS'])
